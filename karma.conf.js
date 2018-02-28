aac7d2ae0f59acec2f3bfaaed2f5f2d8cbd78b64
inho.won <inho.won@newsjel.ly>
2018-02-28 14:21:57 +0900

    test: add and set karma-html

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,13 @@
+const list = require('./demo/list');
+const htmlSrc = list.map(d => { return {src: `./gh-pages/demo/${d.path}.html`, tag: d.path};})
 module.exports = function(config) {
   config.set({
     frameworks: ['browserify', 'tap'],
     files: [
       'http://apis.daum.net/maps/maps3.js?apikey=7fd8127881fe4e612aabd7d3aca98636&libraries=services', //daum api
-      'test/**/*-test.js'
+      'test/**/*-test.js',
+      {pattern: './gh-pages/style/*.css', watched:true, served:true, included:true},
+      {pattern: './dist/jelly.js', watched:false, served:true, included:false}
     ],
     preprocessors: {
       'test/**/*-test.js': [ 'browserify' ]
@@ -13,7 +17,12 @@ module.exports = function(config) {
       transform: [['babelify', { presets: ['env']}]]
     },
     browsers: ['Chrome'],
-    reporters: ['spec'],
+    reporters: ['spec', 'karmaHTML'],
+    client: {
+      karmaHTML: {
+        source: htmlSrc
+      }
+    },
     crossOriginAttribute: false
   });
 };
