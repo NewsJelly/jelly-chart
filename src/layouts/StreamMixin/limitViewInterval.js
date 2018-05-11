05f83f8cc3c2a204101ea9b5f4c6c746a48bfe02
inho.won <inho.won@newsjel.ly>
2018-05-11 16:58:46 +0900

    add multiple attribute to .viewInterval

--- src/layouts/StreamMixin/limitViewInterval.js
+++ src/layouts/StreamMixin/limitViewInterval.js
@@ -1,37 +1,39 @@
 import timeInterval from '../../modules/interval';
 
 function limitViewInterval(scale, domain, isKeep=false) {
-  const viewInterval = this.viewInterval() || domain.length;
-  const intervalType = typeof viewInterval;
+  const viewInterval = this.viewInterval();
+  const intervalUnit = viewInterval ? viewInterval.unit : domain.length;
+  const intervalMultiple = viewInterval ? viewInterval.multiple : 1;
+  const intervalType = typeof intervalUnit;
   scale._defaultDomain = domain;
   if (isKeep) scale._lastDomain = scale.domain();
   if (scale.invert) {
     if (intervalType === 'string' && domain[0] instanceof Date) {
       if (isKeep) {
         domain = [
-          timeInterval[viewInterval].offset(domain[domain.length-1], -1), 
+          timeInterval[intervalUnit].offset(domain[domain.length-1], -1 * intervalMultiple), 
           domain[domain.length-1]
         ];
       } else {
         domain = [
           domain[0], 
-          timeInterval[viewInterval].offset(domain[0], 1)
+          timeInterval[intervalUnit].offset(domain[0], 1 * intervalMultiple)
         ];
       }
     } else if (intervalType === 'number') {
       if (isKeep) {
-        domain = [domain[domain.length-1] - viewInterval, domain[domain.length-1]];
+        domain = [domain[domain.length-1] - intervalUnit * intervalMultiple, domain[domain.length-1]];
       } else {
-        domain = [domain[0], domain[0] + viewInterval];
+        domain = [domain[0], domain[0] + intervalUnit * intervalMultiple];
       }
     }
   } else {
     if (intervalType === 'number') {
       if (isKeep) {
-        const dist = domain.length - viewInterval;
-        domain = domain.slice(dist, dist + viewInterval);
+        const dist = domain.length - intervalUnit* intervalMultiple;
+        domain = domain.slice(dist, dist + intervalUnit * intervalMultiple);
       } else {
-        domain =  domain.slice(0,viewInterval);
+        domain =  domain.slice(0,intervalUnit * intervalMultiple);
       }
     }
   }
--- src/layouts/StreamMixin/viewInerval.js
+++ src/layouts/StreamMixin/viewInerval.js
@@ -1,16 +1,29 @@
 import interval from '../../modules/interval';
 
-function viewInterval(viewInterval) {  
+/**
+ * If unit is number or string, limit the key domain's length or distance within the interval. If the key domain's scale is ordnial, the length of domain will be the same to unit. Also, if the scale is continous, the distance of domain will be fixed as much as unit. If unit is string, it must be a time interval. When multiple exists, it will multiply the unit.
+ * If unit is not specified, returns the existing viewInterval. 
+ * @memberOf StreamMixin#
+ * @function
+ * @example
+ * stream.viewInterval(1000)
+ * stream.viewInterval('month', 3)
+ * @param {number|string} [unit=null] 
+ * @param {number} [multiple=1]
+ * @return {zoom|ZoomMixin}
+ */
+function viewInterval(unit, multiple=1) {  
   if (!arguments.length) return this.__execs__.viewInterval;
-  const type = typeof viewInterval;
+  const type = typeof unit;
   if (type === 'number') {
-    if (viewInterval <= 0) viewInterval = null;
+    if (viewInterval <= 0) unit = null;
   } else if (type === 'string') { //d3-time interval
-    if (!Object.keys(interval).includes(viewInterval)) {
-      viewInterval = null;
+    if (!Object.keys(interval).includes(unit)) {
+      unit = null;
     }
   }
-  this.__execs__.viewInterval = viewInterval;
+  if (unit) this.__execs__.viewInterval = {unit, multiple}
+  else this.__execs__.viewInterval = null;
   return this;
 }
 
