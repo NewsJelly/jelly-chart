c406ff78f4a23f9655b9c9ceba8775c2a01ee214
peikin <rkaehdgns@naver.com>
2019-10-31 23:13:21 +0900

    # bugfix
    - pivot table 데모 파일 수정

--- demo/src/pivot-table/pivot-table/index.js
+++ demo/src/pivot-table/pivot-table/index.js
@@ -1,15 +1,22 @@
-jelly.xyHeatmap().container('#jelly-container')
-  .data([
-    {category:'Blue', name: 'A', value: 10},
-    {category:'Blue', name: 'B', value: 20},
-    {category:'Blue', name: 'C', value: 30},
-    {category:'Blue', name: 'D', value: 40},
-    {category:'Red', name: 'A', value: 20},
-    {category:'Red', name: 'B', value: 10},
-    {category:'Red', name: 'C', value: 40},
-    {category:'Red', name: 'D', value: 10}
-  ]).dimensions(['category', 'name'])
-  .measures('value')
-  .axis('x').axis('y')
-  .legend(true)
-  .render();
+jelly.pivotTable().container('#jelly-container')
+    .data([
+        {category:'Yellow', name: 'A', value: 20},
+        {category:'Yellow', name: 'B', value: 50},
+        {category:'Yellow', name: 'C', value: 10},
+        {category:'Yellow', name: 'D', value: 40},
+        {category:'Green', name: 'A', value: 15},
+        {category:'Green', name: 'B', value: 80},
+        {category:'Green', name: 'C', value: 40},
+        {category:'Green', name: 'D', value: 25},
+        {category:'Blue', name: 'A', value: 70},
+        {category:'Blue', name: 'B', value: 30},
+        {category:'Blue', name: 'C', value: 60},
+        {category:'Blue', name: 'D', value: 35},
+        {category:'Red', name: 'A', value: 20},
+        {category:'Red', name: 'B', value: 10},
+        {category:'Red', name: 'C', value: 40},
+        {category:'Red', name: 'D', value: 10}
+    ]).dimensions(['category', 'name'])
+    .measures('value')
+    .legend(true)
+    .render()
